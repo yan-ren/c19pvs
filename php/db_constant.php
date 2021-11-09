6b4f98c27842f110fa549ea5ebe8f5364e4e1a68
L0veRamen <jasonqc9103@gmail.com>
2021-11-09 11:38:16 -0500

    test qc 123

--- php/db_constant.php
+++ php/db_constant.php
@@ -2,12 +2,14 @@
 
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
+
+// test qc
 DEFINE('DB_USERNAME_LOCAL', 'test');
 DEFINE('DB_PASSWORD_LOCAL', 'password');
 DEFINE('DB_SERVER_LOCAL', '127.0.0.1:3306');
 DEFINE('DB_NAME_LOCAL', 'knc353_2');
 
-DEFINE('DB_USERNAME', 'knc353_2');
-DEFINE('DB_PASSWORD', '65026502');
-DEFINE('DB_SERVER', 'knc353.encs.concordia.ca:3306');
-DEFINE('DB_NAME', 'knc353_2');
+//DEFINE('DB_USERNAME', 'knc353_2');
+//DEFINE('DB_PASSWORD', '65026502');
+//DEFINE('DB_SERVER', 'knc353.encs.concordia.ca:3306');
+//DEFINE('DB_NAME', 'knc353_2');
