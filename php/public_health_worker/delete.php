9b2e283e7b97d27d1151cd637f8ff84e9af6bb01
L0veRamen <jasonqc9103@gmail.com>
2021-11-12 19:19:37 -0500

    create.php progressing

--- php/public_health_worker/delete.php
+++ php/public_health_worker/delete.php
@@ -3,20 +3,20 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 
 // Process delete operation after confirmation
-if (isset($_POST["age_group_id"]) && !empty($_POST["age_group_id"])) {
+if (isset($_POST["person_id"]) && !empty($_POST["person_id"])) {
     // Include config file
     require_once "../config.php";
     $link = connect();
 
     // Prepare a delete statement
-    $sql = "DELETE FROM age_group WHERE age_group_id = ?";
+    $sql = "DELETE FROM person_id WHERE person_id = ?";
 
     if ($stmt = mysqli_prepare($link, $sql)) {
         // Bind variables to the prepared statement as parameters, i stands for integer
         mysqli_stmt_bind_param($stmt, "i", $param_id);
 
         // Set parameters
-        $param_id = (int)($_POST["age_group_id"]);
+        $param_id = (int)($_POST["person_id"]);
 
         // Attempt to execute the prepared statement
         if (mysqli_stmt_execute($stmt)) {
@@ -35,7 +35,7 @@ if (isset($_POST["age_group_id"]) && !empty($_POST["age_group_id"])) {
     mysqli_close($link);
 } else {
     // Check existence of id parameter
-    if (empty(trim($_GET["age_group_id"]))) {
+    if (empty(trim($_GET["person_id"]))) {
         // URL doesn't contain id parameter. Redirect to error page
         header("location: error.php");
         exit();
@@ -66,11 +66,11 @@ if (isset($_POST["age_group_id"]) && !empty($_POST["age_group_id"])) {
                     <h2 class="mt-5 mb-3">Delete Record</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
-                            <input type="hidden" name="age_group_id" value="<?php echo trim($_GET["age_group_id"]); ?>" />
-                            <p>Are you sure you want to delete this age group record?</p>
+                            <input type="hidden" name="age_group_id" value="<?php echo trim($_GET["person_id"]); ?>" />
+                            <p>Are you sure you want to delete this Public Health Worker record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="age_group.php" class="btn btn-secondary">No</a>
+                                <a href="public_health_worker.php" class="btn btn-secondary">No</a>
                             </p>
                         </div>
                     </form>
