0b549e659c6539b1937d809eded92bdc8bc41528
Yan Ren <ryan.ren3@gmail.com>
2021-11-16 00:38:34 -0500

    UI improve (#5)
    * remove home page table display
    
    * modify home page

--- report.php
+++ report.php
@@ -6,31 +6,7 @@
 </head>
 
 <body>
-  <!-- Fixed navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="index.php">C19PVS</a>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarText">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="booking.php">Booking</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="vaccine.php">Vaccine</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="report.php">Report</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="search.php">Search</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
 
-  <?php include "./html/footer.html" ?>
 </body>
 
 </html>
