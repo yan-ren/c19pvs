47443806a2a3469c2990346cde30325b441e6a74
L0veRamen <jasonqc9103@gmail.com>
2021-11-16 10:25:57 -0500

    assignment start

---
+++ php/health_worker_assignment/assignment.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/html/head.html") ?>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Public Health Worker Assignment</h2>
+                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Assignment</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "../config.php";
+                    $link = connect();
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM healthcare_worker_assignment ORDER BY person_id ASC";
+                    if ($result = mysqli_query($link, $sql)) {
+                        if (mysqli_num_rows($result) > 0) {
+                            echo '<table class="table table-bordered table-striped">';
+                            echo "<thead>";
+                            echo "<tr>";
+                            echo "<th>Person_id</th>";
+                            echo "<th>Facility_name</th>";
+                            echo "<th>Start Date</th>";
+                            echo "<th>End Date</th>";
+                            echo "<th>Role</th>";
+                            echo "<th>Vaccine Name</th>";
+                            echo "<th>Dose Given</th>";
+                            echo "<th>Lot</th>";
+                            echo "<th>Action</th>";
+                            echo "</tr>";
+                            echo "</thead>";
+                            echo "<tbody>";
+                            while ($row = mysqli_fetch_array($result)) {
+                                echo "<tr>";
+                                echo "<td>" . $row['person_id'] . "</td>";
+                                echo "<td>" . $row['facility_name'] . "</td>";
+                                echo "<td>" . $row['start_date'] . "</td>";
+                                echo "<td>" . $row['end_date'] . "</td>";
+                                echo "<td>" . $row['role'] . "</td>";
+                                echo "<td>" . $row['vaccine_name'] . "</td>";
+                                echo "<td>" . $row['dose_given'] . "</td>";
+                                echo "<td>" . $row['lot'] . "</td>";
+
+                                echo "<td>";
+                                echo '<a href="read.php?person_id=' . $row['person_id'] . '&facility_name=' . $row['facility_name'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                echo '<a href="update.php?person_id=' . $row['person_id'] .'&facility_name=' . $row['facility_name'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                echo '<a href="delete.php?person_id=' . $row['person_id'] . '&facility_name=' . $row['facility_name'] . '"class="mr-3" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                echo "</td>";
+                                echo "</tr>";
+                            }
+                            echo "</tbody>";
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else {
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else {
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+
+                    // Close connection
+                    mysqli_close($link);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ php/health_worker_assignment/create.php
@@ -0,0 +1,181 @@
+<?php
+// Include config file
+require_once "../config.php";
+require_once "../util.php";
+$link = connect();
+
+// Define variables and initialize with empty values
+$person_id = $facility_name = $start_date = $end_date = $role = $vaccine_name = $dose = $lot = "";
+$start_date = $end_date_error = $role_error = $lot_error = "";
+
+//get all facilities
+$sql = "SELECT * FROM facility";
+$result = mysqli_query($link, $sql);
+$all_facility = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//get all safe vaccine for assignment
+$sql2 = "SELECT * FROM vaccine where status = 'safe'";
+$result2 = mysqli_query($link, $sql2);
+$all_vaccines = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+
+//get all roles 
+$sql3 = "SELECT DISTINCT `role` FROM healthcare_worker_assignment";
+$result3 = mysqli_query($link, $sql3);
+$all_roles = mysqli_fetch_all($result3, MYSQLI_ASSOC);
+
+// get all doses
+$sql4 = "SELECT DISTINCT IFNULL(dose,0) as dose FROM vaccine";
+$result4 = mysqli_query($link, $sql4);
+$all_doses = mysqli_fetch_all($result4, MYSQLI_ASSOC);
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $person_id = (int)trim($_POST["person_id"]);
+    $facility_name = trim($_POST["facility_name"]);
+    $start_date = trim($_POST["start_date"]);
+    $end_date = trim($_POST["end_date"]);
+    $role = trim($_POST["role"]);
+    $vaccine_name = trim($_POST["vaccine_name"]);
+    $dose = trim($_POST["dose_given"]);
+    $lot = trim($_POST["lot"]);
+
+    // Validate person_id employee_id
+  
+    if (empty($employee_id) && $employee_id !== '0') {
+        $employee_id_error = "Please enter a valid employee id";
+    }
+   
+    if (empty($hourly_rate) && $hourly_rate !== '0') {
+        $hourly_rate_error = "Please enter a valid hourly rate";
+    }
+
+    // Check input errors before inserting in database
+    if (empty($person_id_error) && empty($employee_id_error) && empty($facility_name_error) && empty($hourly_rate_error)) {
+        // Prepare an insert statement
+        $sql = "INSERT INTO healthcare_worker (person_id, employee_id, facility_name, hourly_rate, status) VALUES (?, ?, ?, ?, 'A')";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "iisi", $param_person_id, $param_employee_id, $facility_name, $param_hourly_rate);
+
+            $param_person_id = (int)($person_id);
+            $param_employee_id = (int)($employee_id);
+            $param_hourly_rate = (int)($hourly_rate);
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Records created successfully. Redirect to landing page
+                header("location: public_health_worker.php");
+                exit();
+            } else {
+                $error = mysqli_stmt_error($stmt);
+                echo '<script> alert("' . $error . '")</script>';
+            }
+        } else {
+            echo "<script>alert('Oops! Something went wrong. Please try again later. Error:" . $link->error . " ');location='create.php';</script>";
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper {
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Create Public Health Worker Assignemnt</h2>
+                    <p>Please fill this form and submit to add Public Health Worker Assignment record to the database</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Person ID</label>
+                            <input type="number" name="person_id" class="form-control" value="<?php echo $person_id; ?>">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Facility name</label>
+                            <select class="custom-select" id="inputGroupSelect01" name="facility_name">
+                                <?php
+                                foreach ($all_facility as $facility) {
+                                    echo '<option values=\"' . $facility['name'] . '\">' . $facility['name'] . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Start Date</label>
+                            <input type="date" name="start_date" class="form-control <?php echo (!empty($start_rate_error)) ? 'is-invalid' : ''; ?>" value="<?php echo $start_date; ?>">
+                            <span class="invalid-feedback"><?php echo $start_date_error; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>End Date</label>
+                            <input type="date" name="end_date" class="form-control <?php echo (!empty($end_date_error)) ? 'is-invalid' : ''; ?>" value="<?php echo $end_date; ?>">
+                            <span class="invalid-feedback"><?php echo $end_date_error; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Role</label>
+                            <select class="custom-select" id="inputGroupSelect01" name="role">
+                                <?php
+                                foreach ($all_roles as $roles) {
+                                    echo '<option values=\"' . $roles['role'] . '\">' . $roles['role'] . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Vaccine Name</label>
+                            <select class="custom-select" id="inputGroupSelect01" name="vaccine_name">
+                                <?php
+                                foreach ($all_vaccines as $vaccine) {
+                                    echo '<option values=\"' . $vaccine['vaccine_name'] . '\">' . $vaccine['vaccine_name'] . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Dose Given</label>
+                            <select class="custom-select" id="inputGroupSelect01" name="dose_given">
+                                <?php
+                                foreach ($all_doses as $doses) {
+                                    echo '<option values=\"' . $doses['dose'] . '\">' . $doses['dose'] . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Lot ID</label>
+                            <input type="text" name="lot" class="form-control" value="<?php echo $lot; ?>">
+                        </div>
+
+                        
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="assignment.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
