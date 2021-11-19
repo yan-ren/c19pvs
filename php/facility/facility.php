239aef16eb37588e91124e10f31e6c37f2643b90
caizhangbin <caizhangbin@gmail.com>
2021-11-19 11:23:18 -0500

    rebase

---
+++ php/facility/facility.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/html/head.html") ?>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Public Health Facility</h2>
+                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Facility</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "../config.php";
+                    $link = connect();
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM facility";
+                    if ($result = mysqli_query($link, $sql)) {
+                        if (mysqli_num_rows($result) > 0) {
+                            echo '<table class="table table-bordered table-striped">';
+                            echo "<thead>";
+                            echo "<tr>";
+                            echo "<th>Facility Name</th>";
+                            echo "<th>Address</th>";
+                            echo "<th>Phone</th>";
+                            echo "<th>Website</th>";
+                            echo "<th>Type</th>";
+                            echo "<th>Capacity</th>";
+                            echo "<th>Manager ID</th>";
+                            echo "<th>Province</th>";
+                            echo "<th>Category</th>";
+                            echo "</tr>";
+                            echo "</thead>";
+                            echo "<tbody>";
+                            while ($row = mysqli_fetch_array($result)) {
+                                echo "<tr>";
+                                echo "<td>" . $row['name'] . "</td>";
+                                echo "<td>" . $row['address'] . "</td>";
+                                echo "<td>" . $row['phone'] . "</td>";
+                                echo "<td>" . $row['website'] . "</td>";
+                                echo "<td>" . $row['type'] . "</td>";
+                                echo "<td>" . $row['capacity'] . "</td>";
+                                echo "<td>" . $row['manager'] . "</td>";
+                                echo "<td>" . $row['province'] . "</td>";
+                                echo "<td>" . $row['category'] . "</td>";
+                                echo "<td>";
+                                echo '<a href="read.php?name=' . $row['name'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                echo '<a href="update.php?name=' . $row['name'] . '"" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                echo '<a href="delete.php?name=' . $row['name'] . '"" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                echo "</td>";
+                                echo "</tr>";
+                            }
+                            echo "</tbody>";
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else {
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else {
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+
+                    // Close connection
+                    mysqli_close($link);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
